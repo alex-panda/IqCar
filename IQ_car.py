ac74adb7a708428738c373accddfe71effc70b0a
cford <kcford26@gmail.com>
2024-03-07 16:28:07 -0600

    edge_detection

--- IQ_car.py
+++ IQ_car.py
@@ -2,6 +2,8 @@ import argparse
 from runTests import run_tests
 from PIL import Image
 import numpy as np
+from skimage import filters, feature
+import matplotlib.pyplot as plt
 
 def runHw4():
     parser = argparse.ArgumentParser(
@@ -13,7 +15,8 @@ def runHw4():
 
     # Call test harness
     fun_handles = {
-        'foo' : bar
+        'foo' : bar,
+        'edge_detect' : edge_detection
     }
     run_tests(args.function_name, fun_handles)
 
@@ -33,6 +36,34 @@ def edge_detection():
     """
     Get orentation/placement of the gameboard (as well as its exit).
     """
+    fig, axs = plt.subplots(2, 2)
+    img = Image.open('data/IMG_1.png')
+    gray_img = img.convert('L')
+    axs[0, 0].imshow(img)
+    axs[0, 0].set_title('Color Image')
+    axs[0, 1].imshow(gray_img, cmap='gray')
+    axs[0, 1].set_title('Grayscale Image')
+
+    img = np.array(img)
+    gray_img = np.array(gray_img)
+
+    # Sobel edge detection
+    thresh = 0.20
+    edge_img = filters.sobel(gray_img) > thresh
+    axs[1, 0].imshow(edge_img, cmap='gray')
+    axs[1, 0].set_title('Sobel Edge Detection')
+
+    # Canny edge detection
+    sig = 2
+    l_thresh = 50
+    h_thresh = 60
+    edge_img = feature.canny(gray_img, sigma=sig, low_threshold=l_thresh, high_threshold=h_thresh)
+    axs[1,1].imshow(edge_img, cmap='gray')
+    axs[1,1].set_title('Canny Edge Detection')
+
+    plt.savefig('outputs/hello_edges.png')
+    plt.show()
+
 
 def segmentation():
     """
