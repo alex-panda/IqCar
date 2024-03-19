74c671ecbe356132671ce8502a48f8f9784ab162
Jack Stanek <stanekjack@gmail.com>
2024-03-18 20:29:58 -0500

    wip: QoL improvements for BoardState class

--- iqcar/solver.py
+++ iqcar/solver.py
@@ -4,19 +4,22 @@ from collections.abc import Generator
 import ctypes
 
 
+bitboard = ctypes.c_uint64
+
+
 class BoardState(ctypes.Structure):
     """State of some game board"""
     _fields_ = [
         ("n_cars", ctypes.c_size_t),
-        ("horiz", ctypes.c_uint64),
-        ("vert", ctypes.c_uint64)
+        ("horiz", bitboard),
+        ("vert", bitboard)
     ]
 
     # Length of the side of the board.
     BOARD_SIZE = 6
 
     @property
-    def state(self) -> ctypes.c_uint64:
+    def state(self) -> bitboard:
         """A bitfield with car-occupied squares set to 1 and empty squares set to 0"""
         return self.horiz | self.vert
 
@@ -30,8 +33,50 @@ class BoardState(ctypes.Structure):
         """
         return not bool(self.horiz & self.vert)
 
+    @classmethod
+    def single_car(
+            cls,
+            pos: tuple[int, int],
+            length: int,
+            *,
+            horiz: bool
+    ) -> "BoardState":
+        """Create a BoardState with a single car.
+
+        The given position is the top left corner of the car. If the car is too
+        long to fit on the board given its position, a ValueError is raised.
+        """
+        x, y = pos
+        start_idx = y * cls.BOARD_SIZE + x
+        if horiz:
+            if x + length > cls.BOARD_SIZE:
+                raise ValueError(f"Car is too long for horizontal position "
+                                 f"(position: {pos}, length: {length}, "
+                                 "board size: {cls.BOARD_SIZE}")
+            return cls(1, 2 ** (start_idx + length) - (2 ** start_idx), 0)
+        if y + length > cls.BOARD_SIZE:
+            raise ValueError(f"Car is too long for vertical position "
+                             f"(position: {pos}, length: {length}, "
+                             "board size: {cls.BOARD_SIZE}")
+        raise NotImplementedError
+
     def plies(self) -> Generator["BoardState", None, None]:
-        pass
+        """Iterate over all single-move perturbations of the game state"""
+        raise NotImplementedError
+
+    def __str__(self) -> str:
+        line = "+-+-+-+-+-+-+\n"
+        buf = str(line)
+        for y in range(self.BOARD_SIZE):
+            buf += "|"
+            for x in range(self.BOARD_SIZE):
+                if (2 ** (y * self.BOARD_SIZE + x)) & self.state:
+                    buf += "o"
+                else:
+                    buf += " "
+                buf += "|"
+            buf += "\n" + line
+        return buf
 
 
 class Solver:
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -5,10 +5,16 @@ from iqcar.solver import BoardState
 class TestBoardState:
     def test_is_valid(self):
         """Test that valid board states are detected"""
-        s = BoardState(3, 8320)
+        s = BoardState(2, 3, 8320)
         assert s.is_valid()
 
     def test_is_not_valid(self):
         """Test that invalid board states are detected"""
-        s = BoardState(3, 129)
+        s = BoardState(2, 3, 67)
         assert not s.is_valid()
+
+    def test_creating_single_car(self):
+        """Test creating a single car board"""
+        s = BoardState.single_car((0, 1), 2, horiz=True)
+        print(f"{s.horiz:b}")
+        assert s.horiz == 3
